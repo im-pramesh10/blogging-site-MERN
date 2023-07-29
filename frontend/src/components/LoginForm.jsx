ac8bf07edfa0209b79edd8d1c1b05693b77b9219
Pramesh Mahat <cruisepramesh@gmail.com>
2023-07-29 19:19:24 +0545

    axios api request to login using jwt tokens

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,14 +1,37 @@
 import {useFormik} from "formik";
 import {loginFormSchema} from "../schemas/loginFormSchema";
+import axios from "axios";
 
 const LoginForm = () => {
-    const onSubmit = (values, actions) => {
-        console.log(values)
-        console.log(actions) // do your api request to login here and clear the form
-        //also display the error message if some thing goes wrong
+    const onSubmit = async (values, actions) => {
+        console.log(actions)
+        // do your api request to login here and clear the form
+        // also display the error message if some thing goes wrong
+        try {
+            const result = await axios({
+                method: 'post',
+                url: 'http://localhost:8080/auth/login',
+                data: {
+                    usernameOrEmail: values.usernameOrEmail,
+                    pwd: values.password
+                }
+            });
+            const data = result.data;
+            console.log(data);
+        } catch (error) {
+            alert(error.response.data.message || error.response.data || error.message)
+        }
+        actions.resetForm();
     }
 
-    const {handleChange, handleSubmit, values, errors} = useFormik({
+    const {
+        handleChange,
+        touched,
+        handleSubmit,
+        handleBlur,
+        values,
+        errors
+    } = useFormik({
         initialValues: {
             usernameOrEmail: "",
             password: ""
@@ -22,19 +45,31 @@ const LoginForm = () => {
             <form onSubmit={handleSubmit}
                 className="flex flex-wrap flex-col items-center w-[4.15in] h-[4.97in] bg-gray2 rounded-[68px] shadow-glow">
                 <p className=" text-yellow italic font-[1000] text-4xl mt-[0.6in]">Blog !t</p>
-                <p className="mt-[0.5in] text-2xl text-[#b2b2b2]">Username/Email</p>
+                <p className="mt-[0.5in] text-2xl text-[#b2b2b2]">Username/Email&nbsp;{
+                    errors.usernameOrEmail && touched.usernameOrEmail ? <span className="text-[#cc241d] text-base">
+                        *{
+                        errors.usernameOrEmail
+                    }</span> : null
+                }</p>
                 <input name="usernameOrEmail"
                     value={
                         values.usernameOrEmail
                     }
                     onChange={handleChange}
+                    onBlur={handleBlur}
                     type="text"
                     className="p-3 mt-[0.07in] text-2xl text-[#ffffff] w-[16rem] h-[3.3rem] bg-[#333333] rounded-lg"/>
-                <p className="mt-[0.2in] text-2xl text-[#b2b2b2]">Password</p>
+                <p className="mt-[0.2in] text-2xl text-[#b2b2b2]">Password&nbsp;{
+                    errors.password && touched.password ? <span className="text-[#cc241d] text-base">
+                        *{
+                        errors.password
+                    }</span> : null
+                }</p>
                 <input name="password"
                     value={
                         values.password
                     }
+                    onBlur={handleBlur}
                     onChange={handleChange}
                     type="password"
                     className="p-3 mt-[0.07in] text-2xl text-[#ffffff] w-[16rem] h-[3.3rem] bg-[#333333] rounded-lg"/>
